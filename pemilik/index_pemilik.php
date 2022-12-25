b93e0a9af56f834d697bc725bbab295ac3a82065
Rizqi Cahya Angelita <90441810+rizqicahyaa@users.noreply.github.com>
2022-12-25 09:54:22 +0700

    Update index_pemilik.php

--- pemilik/index_pemilik.php
+++ pemilik/index_pemilik.php
@@ -304,14 +304,13 @@ $idpemilik = @$_SESSION['pemilik_id'];
       <div
         class="copywrite-text d-flex align-items-center justify-content-center"
       >
-        <p>
-          <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+         <p>
           Copyright &copy;
           <script>
             document.write(new Date().getFullYear());
           </script>
           All rights reserved
-          <i class="fa fa-heart-o" aria-hidden="true"></i> Oleh Kelompok 7
+          <i class="fa fa-heart-o" aria-hidden="true"></i> Oleh Kelompok 8
         </p>
       </div>
     </footer>
